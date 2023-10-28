6b1a832530fb2aec98dec327fc753fc7caf3b771
Felipe <github@fespinoza.net>
2023-10-28 22:03:15 +0100

    Add more unit tests to logic

--- src/logic.rs
+++ src/logic.rs
@@ -173,4 +173,84 @@ mod test {
         assert!(result.is_ok(), "result error: {:?}", result.err());
         Ok(())
     }
+
+    #[tokio::test]
+    async fn test_process_ignore_not_owner_whitelist() -> testresult::TestResult {
+        let mut github = MockGithubApi::new();
+        let kanbanize = MockKanbanizeApi::new();
+        let pull = PullRequest {
+            url: Url::parse("https://example.com/pull")?,
+            head_reference: String::from("request-123"),
+            repository_owner: String::from("not owner"),
+            is_private: true,
+        };
+        github.expect_get_issues().return_once(|| {
+            Ok(vec![Issue {
+                url: Url::parse("https://example.com/issue")?,
+                body: String::default(),
+                pull_request_url: Some(Url::parse("https://example.com/pull")?),
+            }])
+        });
+
+        let pull1 = pull.clone();
+        github
+            .expect_get_pull_from_url()
+            .with(mockall::predicate::eq(pull.url.clone()))
+            .return_once(move |_| Ok(pull1));
+
+        let logic = Service::new(Box::new(github), Box::new(kanbanize), "owner".to_string());
+        let result = logic.process().await;
+        assert!(result.is_ok(), "result error: {:?}", result.err());
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_process_ignore_public() -> testresult::TestResult {
+        let mut github = MockGithubApi::new();
+        let kanbanize = MockKanbanizeApi::new();
+        let pull = PullRequest {
+            url: Url::parse("https://example.com/pull")?,
+            head_reference: String::from("request-123"),
+            repository_owner: String::from("owner"),
+            is_private: false,
+        };
+        github.expect_get_issues().return_once(|| {
+            Ok(vec![Issue {
+                url: Url::parse("https://example.com/issue")?,
+                body: String::default(),
+                pull_request_url: Some(Url::parse("https://example.com/pull")?),
+            }])
+        });
+
+        let pull1 = pull.clone();
+        github
+            .expect_get_pull_from_url()
+            .with(mockall::predicate::eq(Url::parse(
+                "https://example.com/pull",
+            )?))
+            .return_once(move |_| Ok(pull1));
+
+        let logic = Service::new(Box::new(github), Box::new(kanbanize), "owner".to_string());
+        let result = logic.process().await;
+        assert!(result.is_ok(), "result error: {:?}", result.err());
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_process_ignore_pull_with_body() -> testresult::TestResult {
+        let mut github = MockGithubApi::new();
+        let kanbanize = MockKanbanizeApi::new();
+        github.expect_get_issues().return_once(|| {
+            Ok(vec![Issue {
+                url: Url::parse("https://example.com/issue")?,
+                body: String::from("existing"),
+                pull_request_url: Some(Url::parse("https://example.com/pull")?),
+            }])
+        });
+
+        let logic = Service::new(Box::new(github), Box::new(kanbanize), "owner".to_string());
+        let result = logic.process().await;
+        assert!(result.is_ok(), "result error: {:?}", result.err());
+        Ok(())
+    }
 }
